8dfeeee9e0d5eca51d700f5abb8e8f056da69932
Nicholas Simonelli <nrsimonelli@gmail.com>
2020-06-21 12:05:56 -0500

    got fields to push to array and clear

--- scripts/client.js
+++ scripts/client.js
@@ -1,3 +1,50 @@
 console.log('client online');
 
+const employeeArray = [];
+
+$(document).ready(onReady);
+
+function onReady(){
+    console.log('jQ online');
+    clickListen();
+
+}
+
+
+function clickListen() {
+    $('#submitButton').on('click', pushData);
+    $('#deleteButton').on('click', removeData);
+
+    
+}   
+
+function pushData() {
+    const newEntry = {
+        first: $('#firstName').val(),
+        last: $('#lastName').val(),
+        id: $('#idNumber').val(),
+        title: $('#jobTitle').val(),
+        salary: $('#annualSalary').val()
+    }
+
+    // push to array
+    employeeArray.push(newEntry);
+
+    // clears input fields
+    $('#firstName').val('');
+    $('#lastName').val('');
+    $('#idNumber').val('');
+    $('#jobTitle').val('');
+    $('#annualSalary').val('');
+
+    return true;
+}
+
+function removeData() {
+    
+}
+
+
+//handle click event
+//event.preventDefault();
 
